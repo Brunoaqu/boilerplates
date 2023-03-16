ff2c5747bbed844a2cb16b25ec453aec1337e9fd
Bruno Aquino <bruno.gl.aquino@gmail.com>
2023-03-16 12:19:15 -0300

    feat: added install docker in ubuntu script

--- scripts/install_docker/ubuntu.sh
+++ scripts/install_docker/ubuntu.sh
@@ -1,31 +1,21 @@
 # Remove old if exists
-
-sudo apt-get remove docker docker-engine docker.io containerd runc
+apt remove docker docker-engine docker.io containerd runc -y
 
 # Update the apt package index and install packages to allow apt to use a repositoryover HTTPS
-
-sudo apt update
-sudo apt install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+apt update -y
+apt install ca-certificates curl gnupg lsb-release -y
 
 # Add Docker’s official GPG key
-
-sudo mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 # Use the following command to set up the repository
-
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Update the apt package index
-
-sudo apt update
+apt update -y
 
 # Install docker latest release
-
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
